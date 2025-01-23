5de450758172abdbe657699644a7b9da5d9654d5
RaphaelIsaiah <rafvie12@gmail.com>
2025-01-23 11:27:21 +0100

    Add Redux setup and initial QuoteBox component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+// import React from "react";
+import { Provider } from "react-redux";
+import QuoteBox from "./components/QuoteBox";
 import "./App.css";
+import store from "./redux/store/store";
 
 function App() {
-  return <></>;
+  return (
+    <Provider className="App" store={store}>
+      <QuoteBox />
+    </Provider>
+  );
 }
 
 export default App;
