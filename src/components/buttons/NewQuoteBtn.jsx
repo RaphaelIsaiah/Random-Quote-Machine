c10dc47b89cebc9b315282729d5b437bfcfc1ba8
RaphaelIsaiah <rafvie12@gmail.com>
2025-02-03 06:25:46 +0100

    Refactor button components to remove unused hover state logic and clean up CSS styles

--- src/components/buttons/NewQuoteBtn.jsx
+++ src/components/buttons/NewQuoteBtn.jsx
@@ -1,34 +1,37 @@
-import { useState, useRef } from "react";
+import { useRef } from "react";
 import PropTypes from "prop-types";
 
 const NewQuoteBtn = ({ onClick }) => {
-  const [isHovered, setIsHovered] = useState(false);
+  // const [isHovered, setIsHovered] = useState(false);
   const buttonRef = useRef(null);
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
+  // const handleMouseEnter = () => {
+  //   setIsHovered(true);
+  // };
 
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
+  // const handleMouseLeave = () => {
+  //   setIsHovered(false);
+  // };
 
   const handleClick = () => {
-    setIsHovered(false); // Reset the hover state on click
+    // setIsHovered(false); // Reset the hover state on click
     onClick();
+    buttonRef.current.blur();
     // Manually reset the hover state after click
-    if (buttonRef.current) {
-      buttonRef.current.classList.remove("hover");
-    }
+    // if (buttonRef.current) {
+    //   buttonRef.current.blur()
+    //   // buttonRef.current.classList.remove("hover");
+    // }
   };
 
   return (
     <button
       ref={buttonRef}
       id="new-quote"
-      className={`btn primary ${isHovered ? "hover" : ""}`}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
+      // className={`btn primary ${isHovered ? "hover" : ""}`}
+      className="btn primary"
+      // onMouseEnter={handleMouseEnter}
+      // onMouseLeave={handleMouseLeave}
       onClick={handleClick}
     >
       New Quote
--- src/components/buttons/TwitterBtn.jsx
+++ src/components/buttons/TwitterBtn.jsx
@@ -1,34 +1,36 @@
-import { useState, useRef } from "react";
+import { useRef } from "react";
 import PropTypes from "prop-types";
 
 const TwitterBtn = ({ url }) => {
-  const [isHovered, setIsHovered] = useState(false);
+//   const [isHovered, setIsHovered] = useState(false);
   const buttonRef = useRef(null);
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
+//   const handleMouseEnter = () => {
+//     setIsHovered(true);
+//   };
 
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
+//   const handleMouseLeave = () => {
+//     setIsHovered(false);
+//   };
 
   const handleClick = () => {
-    setIsHovered(false);
-    if (buttonRef.current) {
-      buttonRef.current.classList.remove("hover");
-    }
+    buttonRef.current.blur()
+    // setIsHovered(false);
+    // if (buttonRef.current) {
+    //   buttonRef.current.classList.remove("hover");
+    // }
   };
 
   return (
     <a
       href={url}
       id="tweet-quote"
-      className={`btn secondary ${isHovered ? "hover" : ""}`}
+    //   className={`btn secondary ${isHovered ? "hover" : ""}`}
+    className="btn secondary"
       target="_blank"
       rel="noopener noreferrer"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
+    //   onMouseEnter={handleMouseEnter}
+    //   onMouseLeave={handleMouseLeave}
       onClick={handleClick}
     >
       <i className="fa-brands fa-x-twitter"></i> Tweet
